8100dcfa17fe3786eb609998d6c3b4c09ccafca3
petergyf <guoyf1@shanghaitech.edu.cn>
2022-05-07 01:01:12 -0400

    delete files

--- helper.py
+++
@@ -1,7 +0,0 @@
-TASK = ['en-de', 'en-fr', 'en-zh']
-LANG = ['en', 'de', 'fr']
-LANG_MODEL = {'en': 'en_core_web_sm',
-              'de': 'de_core_news_sm', 'fr': 'fr_core_news_sm'}
-
-special_symbols = ['<unk>', '<pad>', '<bos>', '<eos>']
-UNK_IDX, PAD_IDX, BOS_IDX, EOS_IDX = 0, 1, 2, 3
